2c882b3aa9295af12907d7e7c81672964bdcf46d
Patel Yugant <yugant177@gmail.com>
2025-09-09 01:11:12 +0530

    Add files via upload

---
+++ checkweatherGorN.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class checkweatherGorN{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number");
+		int a = sc.nextInt();
+		System.out.println(a>8);
+		System.out.println(7*49 / 7 + 35/7);
+		
+	}
+}
