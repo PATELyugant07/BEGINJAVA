56443bd69aff68279b160019a45682d76aa00ae6
Patel Yugant <yugant177@gmail.com>
2025-09-08 10:56:26 +0530

    Add files via upload

---
+++ OperatorsD2.java
@@ -0,0 +1,10 @@
+public class OperatorsD2 {
+	public static void main(String[] args){
+		int a = 2;
+		int b = 10 - a;
+		b += 14;
+		System.out.println(b);
+		System.out.println(10<18); // comparison operator
+		System.out.println(16>13  && 16>13);
+	}
+}
---
+++ President.java
@@ -0,0 +1,9 @@
+public class President{
+	public static void main(String[] args){
+	int x = 6; 
+	int y = 1;
+	int z = 4;
+	int k = y*y - (4*x*z)/(2*x);
+	System.out.println(k);
+	}
+}
