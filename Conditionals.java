bef03dbafd85fe82686163c9dd5a977d3c6a8017
Patel Yugant <yugant177@gmail.com>
2025-09-10 23:22:22 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,12 @@
+public class Conditionals{
+	public static void main(String[] args){
+		int age =20 ; 
+		if(age>18)
+		{
+			System.out.println("Yes Boy You Can Drive!!!");
+		}
+		else{
+			System.out.println("No Boy You Can't Be Drive!!!!");
+		}
+	}
+}
