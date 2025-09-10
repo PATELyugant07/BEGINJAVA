1cc563f6cd7bed200fd9ed850c8618e66c4cfbe1
Patel Yugant <yugant177@gmail.com>
2025-09-10 23:43:30 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,13 @@
+public class Logical{
+	public static void main(String[] args){
+		boolean a = false; 
+		boolean b = true;
+		if (a && b)
+		{
+			System.out.println("Y");
+		}
+		else{
+			System.out.println("N");
+		}
+	}
+}
