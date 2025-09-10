60bb862b8548c7c22b323fee979114407d346dbb
Patel Yugant <yugant177@gmail.com>
2025-09-10 23:47:58 +0530

    Add files via upload

---
+++ LogicalOr.java
@@ -0,0 +1,13 @@
+public class LogicalOr{
+	public static void main(String[] args){
+		boolean a = false; 
+		boolean b = false;
+		if (a || b)
+		{
+			System.out.println("Y");
+		}
+		else{
+			System.out.println("N");
+		}
+	}
+}
