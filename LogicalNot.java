0c1c933403bd547c18969f4bfdcc301e9203101f
Patel Yugant <yugant177@gmail.com>
2025-09-10 23:58:20 +0530

    Add files via upload

---
+++ LogicalNot.java
@@ -0,0 +1,11 @@
+public class LogicalNot{
+	public static void main(String[] args){
+		boolean a = false; 
+		boolean b = true;
+		System.out.println("For Logical Not");
+		System.out.print("Not(a) is ");
+		System.out.println(!a);
+		System.out.print("Not(b)");
+		System.out.println(!b);
+	}
+}
