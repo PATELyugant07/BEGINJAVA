5b757290a630f06f245fe8634d3b8c9fa9e84c58
Patel Yugant <yugant177@gmail.com>
2025-09-11 23:12:54 +0530

    Add files via upload

---
+++ PracticeIFElse.java
@@ -0,0 +1,10 @@
+public class PracticeIFElse{
+	public static void main(String[] args){
+		int a = 10;
+		if (a == 11){
+			System.out.println("I am 11");
+		} else{
+			System.out.println("I am Not 11");
+		}
+	}
+}
