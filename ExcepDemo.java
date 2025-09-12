41f786548f2d4ed60f0478fdd5a9077c76346e5c
Patel Yugant <yugant177@gmail.com>
2025-09-12 12:45:40 +0530

    Add files via upload

---
+++ ExcepDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ExcepDemo{
+	public static void main(String[] args)
+	{
+		int n=10/2;
+		System.out.println("division ="+n);
+		
+		int a[]=new int[3];
+		a[5]=10;
+		System.out.println("A[5] ="+a[5]);
+	}
+}
