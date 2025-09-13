6f0173ea115528a6f7228db814a5f5fce4f4663c
Patel Yugant <yugant177@gmail.com>
2025-09-13 19:17:20 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,9 @@
+public class Dowhile{
+	public static void main(String[] args){
+		int b = 0;
+		do{
+			System.out.println(b);
+			b++;
+		}while(b<5);
+	}
+}
---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop{
+	public static void main(String[] args){
+		int n = 6;
+		for (int a=0; a<=5; a++ )
+		{
+			System.out.println(2*a+1);
+		}
+	}
+}
