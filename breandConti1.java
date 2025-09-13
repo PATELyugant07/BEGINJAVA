c86bba098da4cd143c25989359a5a60699a332aa
Patel Yugant <yugant177@gmail.com>
2025-09-13 23:51:27 +0530

    Add files via upload

---
+++ breandConti1.java
@@ -0,0 +1,12 @@
+public class breandConti1{
+	public static void main(String[] args){
+		for(int i=0;i<5;i++){
+			if(i==2){
+				System.out.println("ending the loop");
+				continue;
+			}
+			System.out.println(i);
+			System.out.println("java is great");
+		}
+	}
+}
