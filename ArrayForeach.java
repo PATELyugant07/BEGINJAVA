75dbc9161cad48ad71d9bbc0bfc08295b4de5ba0
Patel Yugant <yugant177@gmail.com>
2025-09-16 01:47:28 +0530

    Add files via upload

---
+++ ArrayForeach.java
@@ -0,0 +1,14 @@
+public class ArrayForeach{
+	public static void main(String[] args){
+		int [] mark = new int[5];
+		mark[0] = 100;
+		mark[1] = 70;
+		mark[2] = 30;
+		mark[3] = 40;
+		mark[4] = 60;
+		for (int element: mark )
+		{
+			System.out.println(element);
+		}
+	}
+}
