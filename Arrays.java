b11927a08b833d3afda33ee35e83458a420a6650
Patel Yugant <yugant177@gmail.com>
2025-09-15 23:08:59 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays{
+	public static void main(String[] args){
+		int [] mark = new int[5];
+		mark[0] = 100;
+		mark[1] = 70;
+		mark[2] = 30;
+		mark[3] = 40;
+		mark[4] = 60;
+		System.out.println(mark[4]);
+	}
+}
