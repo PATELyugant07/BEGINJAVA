ab6f262e8e2f8cc9f436cc16595d2e70d6adb16f
Patel Yugant <yugant177@gmail.com>
2025-09-17 00:03:31 +0530

    Add files via upload

---
+++ Arrayaverage.java
@@ -0,0 +1,11 @@
+public class Arrayaverage{
+	public static void main(String[] args){
+		float [] marks= {45.5f, 55.5f, 65.5f, 75.5f, 85.5f};
+		float sum = 0; 
+		for (float element: marks)
+		{
+			sum = sum + element;
+		}
+		System.out.println("The Value of avg mark is" +sum/marks.length);
+	}
+}
