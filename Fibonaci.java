4815f7e728bcd19377b7abefcabf08256f9be228
Patel Yugant <yugant177@gmail.com>
2025-09-23 23:54:56 +0530

    Add files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,17 @@
+public class Fibonaci{
+		static int (int n){
+		if(n=1){
+			return 0;
+		}
+		else if (n==2){
+			retun 1;
+		}
+		else{
+			return fib(n-1)+ fib(n-2);
+		}
+	}
+	public static void main(String[] args){
+		int result = fib(n);
+		System.out.println(result);
+	}
+}
