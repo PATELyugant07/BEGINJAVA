e756e0ec228c5f07e51c122567d87055e0610963
Patel Yugant <yugant177@gmail.com>
2025-09-24 23:48:14 +0530

    Add files via upload

--- ExcepDemo.java
+++ ExcepDemo.java
@@ -1,12 +1,34 @@
-import java.util.*;
-class ExcepDemo{
+import java.io.*;
+class ExcepDemo
+{
+	
 	public static void main(String[] args)
 	{
-		int n=10/2;
-		System.out.println("division ="+n);
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));	
+		System.out.println("Enter Name : ");
+		String name=br.readLine();
+		System.out.println("Name : "+name);
+
+		     /*	int n=10/2;
+                System.out.println("Division ="+n);
+				
+				int a[]=new int[5];
+			    a[3]=10;
+				System.out.println("A[3] ="+a[3]);
+				try
+				{
+				FileReader fr=new FileReader("C:\\myfile.txt");	
+				}
+				catch(Exception e)
+				{
+					System.out.println("File is Not availabe "+e);
+				
+				}
+				
+		       */
+		       
+		}
 		
-		int a[]=new int[3];
-		a[5]=10;
-		System.out.println("A[5] ="+a[5]);
-	}
+	
 }
+
